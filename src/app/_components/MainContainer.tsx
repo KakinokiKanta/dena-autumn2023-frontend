4f9b6108ea51db0dcc1dc895eec2c089f3f7f5ee
KakinokiKanta <kanta12621@gmail.com>
2024-06-06 17:41:37 +0900

    ファイルの全削除

--- src/app/_components/MainContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode, FC } from "react";
-
-type Props = { children: ReactNode };
-
-const MainContainer: FC<Props> = ({ children }) => {
-  return <div className="bg-darkder">{children}</div>;
-};
-
-export default MainContainer;
--- src/types/userInfo.ts
+++
@@ -1,5 +0,0 @@
-interface UserInfo {
-  id: string;
-  name: string;
-  is_new: string;
-}
